861e6c2062295cf5a5e655f15d4b3abc2a069696
HarrisonTCodes <htillbackup@gmail.com>
2024-06-01 14:08:17 +0100

    Classname updated

--- frontend/app/components/train/TrainWidget.tsx
+++ frontend/app/components/train/TrainWidget.tsx
@@ -11,7 +11,7 @@ export default function TrainWidget({ platform, fromCrs, departTime, estimatedDe
 
     return (
         <div className="flex border-2 border-gray-300 rounded-xl max-w-[820px] w-[85%] divide-x-2">
-            <section className="flex-1 flex-grow flex flex-col items-center gap-1 ">
+            <section className="flex-1 flex-grow flex flex-col items-center gap-1">
                 <p>{fromCrs} {platform ? `P${platform}` : ""}</p>
                 <p className="text-3xl">{departTime}</p>
                 <p className="text-gray-400">{estimatedDepartTime ? `${estimatedDepartTime}` : "Imminent"}</p>
