7de36766c73d86933cc990e50c3e45245775d325
HarrisonTCodes <htillbackup@gmail.com>
2024-06-01 18:12:28 +0100

    Convert API to accept station names rather than CRS

--- backend/main.py
+++ backend/main.py
@@ -33,24 +33,30 @@ def duration(t1, t2):
     return (datetime.strptime(t2, "%H:%M") - datetime.strptime(t1, "%H:%M")) / 60
 
 #GET service IDs from one station to another
-@app.get("/service-id/{from_crs}/{to_crs}")
-def service_id(from_crs: str, to_crs: str):
-    if not all([crs in stations.values() for crs in (from_crs, to_crs)]):
-        raise HTTPException(status_code=400, detail="Bad request")
+@app.get("/service-id/{from_name}/{to_name}")
+def service_id(from_name: str, to_name: str):
+    #convert station names to crs
+    (from_crs, to_crs) = (stations.get(from_name), stations.get(to_name))
+    if not all((from_crs, to_crs)): raise HTTPException(status_code=400, detail="Bad request")
     
+    #get IDs
     huxley_response = requests.get(f"https://huxley2.azurewebsites.net/departures/{from_crs}/to/{to_crs}/?accessToken={token}")
 
     if huxley_response.status_code == 200:
         train_services = huxley_response.json()["trainServices"]
         service_ids = [train_service["serviceIdPercentEncoded"] for train_service in train_services]
         return service_ids
-    
     else:
         raise HTTPException(status_code=400, detail="Bad request")
 
-#GET service info from ID, plus info about given calling point from crs
-@app.get("/service/{service_id}/{to_crs}")
-def service(service_id: str, to_crs: str):
+#GET service info from ID, plus info about given calling point from station name
+@app.get("/service/{service_id}/{to_name}")
+def service(service_id: str, to_name: str):
+    #convert station name to crs
+    to_crs = stations.get(to_name)
+    if not to_crs: raise HTTPException(status_code=400, detail="Bad request")
+
+    #get service info
     service_response = requests.get(f"https://huxley2.azurewebsites.net/service/{service_id}/?accessToken={token}")
 
     if service_response.status_code == 200:
