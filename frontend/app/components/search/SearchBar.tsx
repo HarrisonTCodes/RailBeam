64f0ed69b58f1fe621b82cb33d9d477a160fd134
HarrisonTCodes <htillbackup@gmail.com>
2024-06-02 11:40:49 +0100

    Added state value to searchbar

--- frontend/app/components/search/SearchBar.tsx
+++ frontend/app/components/search/SearchBar.tsx
@@ -1,10 +1,11 @@
-export default function SearchBar({ label, setState } : { label?: string, setState: (value: string) => void }) {
+export default function SearchBar({ label, setState, state } : { label?: string, setState: (value: string) => void, state: string }) {
     return (
         <input 
             type="text"
             placeholder={label}
             className="border-2 border-gray-300 focus:border-gray-700 focus:outline-none rounded-xl focus:ring-0 py-2 px-2 text-xl w-80 transition"
             onChange={(ev) => setState(ev.target.value)}
+            value={state}
         />
     )
 }
