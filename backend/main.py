93ac2dcc659335eb9cdeee2d6754853c9492a37a
HarrisonTCodes <htillbackup@gmail.com>
2024-06-02 14:39:40 +0100

    Convert user prompt to lowercase

--- backend/main.py
+++ backend/main.py
@@ -99,4 +99,4 @@ def service(service_id: str, to_name: str):
 #get station names that begin with a given prompt
 @app.get("/station/{prompt}")
 def station(prompt: str):
-    return [station_name.title() for station_name in stations.keys() if station_name[0:len(prompt)] == prompt]
+    return [station_name.title() for station_name in stations.keys() if station_name[0:len(prompt)] == prompt.lower()]
