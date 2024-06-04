5818032f36791de014d85e8b31ef16e38b2e20ad
HarrisonTCodes <htillbackup@gmail.com>
2024-06-04 18:21:50 +0100

    Updated slow/cancel color as tailwind config variable

--- frontend/app/components/train/TrainWidget.tsx
+++ frontend/app/components/train/TrainWidget.tsx
@@ -7,7 +7,7 @@ export default function TrainWidget({ platform, fromCrs, departTime, estimatedDe
             <section className="flex-1 flex-grow flex flex-col items-center gap-1">
                 <p>{fromCrs} {platform ? `P${platform}` : ""}</p>
                 <p className="text-3xl">{departTime}</p>
-                <p className={`${cancelled ? "text-[#de0404] font-medium" : "text-gray-400"}`}>
+                <p className={`${cancelled ? "text-failure font-medium" : "text-gray-400"}`}>
                     {estimatedDepartTime ? `${estimatedDepartTime}` : "Imminent"}
                 </p>
             </section>
@@ -15,7 +15,7 @@ export default function TrainWidget({ platform, fromCrs, departTime, estimatedDe
             <section className="flex-1 flex-grow flex flex-col items-center gap-1">
                 <p>{toCrs}</p>
                 <p className="text-3xl">{arriveTime}</p>
-                <p className={`${cancelled ? "text-[#de0404] font-medium" : "text-gray-400"}`}>
+                <p className={`${cancelled ? "text-failure font-medium" : "text-gray-400"}`}>
                     {estimatedArriveTime}
                 </p>
             </section>
@@ -23,7 +23,7 @@ export default function TrainWidget({ platform, fromCrs, departTime, estimatedDe
             <section className="flex-1 flex-grow flex flex-col items-center gap-1">
                 <p>DURATION</p>
                 <p className="text-3xl">{duration.toString()}m</p>
-                <p className={`${duration > averageDuration || cancelled ? "text-[#de0404]" : "text-[#27ad1d]"} font-medium`}>
+                <p className={`${duration > averageDuration || cancelled ? "text-failure" : "text-[#27ad1d]"} font-medium`}>
                     {cancelled ? "Cancelled" : (duration > averageDuration ? "Slow" : "Fast")}
                 </p>
             </section>
