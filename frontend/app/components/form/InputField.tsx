329453c2d588ad1bdadd55dc9f690d384750cd08
HarrisonTCodes <htillbackup@gmail.com>
2024-06-07 18:58:25 +0100

    Changed input field text size

--- frontend/app/components/form/InputField.tsx
+++ frontend/app/components/form/InputField.tsx
@@ -1,7 +1,7 @@
 export default function InputField({ name, placeholder, type } : { name: string, placeholder: string, type?: string }) {
     return (
         <input 
-            className="border-2 border-gray-300 focus:border-gray-700 focus:outline-none rounded-xl focus:ring-0 py-2 px-2 text-xl w-72 transition"
+            className="border-2 border-gray-300 focus:border-gray-700 focus:outline-none rounded-xl focus:ring-0 py-2 px-2 text-lg w-72 transition"
             name={name}
             placeholder={placeholder}
             type={type}
