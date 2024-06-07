ad79e7645094570eafcea6222bce1b8a5084de4f
HarrisonTCodes <htillbackup@gmail.com>
2024-06-07 19:46:56 +0100

    Removed input field

--- frontend/app/components/form/FormButton.tsx
+++ frontend/app/components/form/FormButton.tsx
@@ -1,8 +1,8 @@
 import { ReactElement } from "react";
 
-export default function FormButton({ onClick, label, icon, type } : { onClick?: () => void, label: string, icon?: ReactElement, type?: "submit" }) {
+export default function FormButton({ onClick, label, icon } : { onClick?: () => void, label: string, icon?: ReactElement }) {
     return (
-        <button type={type} onClick={onClick} className="text-lg w-72 flex justify-center items-center rounded-xl p-1 border-2 hover:bg-gray-100 transition gap-4">
+        <button onClick={onClick} className="text-lg w-72 flex justify-center items-center rounded-xl p-1 border-2 hover:bg-gray-100 transition gap-4">
             {icon}
             {label}
         </button>
--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -1,17 +1,31 @@
+"use client";
+
 import { Login, Person } from "@mui/icons-material"
-import InputField from "../components/form/InputField"
 import FormButton from "../components/form/FormButton"
+import { useState } from "react"
 
 export default function LoginPage() {
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+
     return (
         <div className="flex items-center justify-center h-[100vh]">
-            <div className="flex flex-col items-center rounded-xl border-gray-300 border w-[60%] min-w-[300px] max-w-[500px] h-[50%] shadow-lg py-2">
+            <div className="flex flex-col items-center rounded-xl border-gray-300 border w-[60%] min-w-[300px] max-w-[500px] h-[50%] shadow-lg py-2 gap-4">
                 <p className="text-2xl flex items-center gap-2"><Person fontSize="large" /> Login to RailBeam</p>
-                <form action="" className="flex flex-col items-center gap-4 pt-4">
-                    <InputField name="username" placeholder="Username" />
-                    <InputField name="password" placeholder="Password" type="password" />
-                    <FormButton label="Login" icon={<Login fontSize="large" />} type="submit" />
-                </form>
+                <input 
+                    placeholder="Username"
+                    className="border-2 border-gray-300 focus:border-gray-700 focus:outline-none rounded-xl focus:ring-0 py-2 px-2 text-lg w-72 transition"
+                    onChange={(ev) => setUsername(ev.target.value)}
+                    value={username}
+                />
+                <input 
+                    placeholder="Password"
+                    type="password"
+                    className="border-2 border-gray-300 focus:border-gray-700 focus:outline-none rounded-xl focus:ring-0 py-2 px-2 text-lg w-72 transition"
+                    onChange={(ev) => setPassword(ev.target.value)}
+                    value={password}
+                />
+                <FormButton label="Login" icon={<Login fontSize="large" />} />
             </div>
         </div>
     )
