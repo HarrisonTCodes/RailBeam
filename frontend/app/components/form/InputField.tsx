44ef737378596930e5a7ac6a4eafe89949081c8a
HarrisonTCodes <htillbackup@gmail.com>
2024-06-07 18:57:27 +0100

    Created input fields

---
+++ frontend/app/components/form/InputField.tsx
@@ -0,0 +1,10 @@
+export default function InputField({ name, placeholder, type } : { name: string, placeholder: string, type?: string }) {
+    return (
+        <input 
+            className="border-2 border-gray-300 focus:border-gray-700 focus:outline-none rounded-xl focus:ring-0 py-2 px-2 text-xl w-72 transition"
+            name={name}
+            placeholder={placeholder}
+            type={type}
+        />
+    )
+}
--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -1,8 +1,15 @@
-export default function Login() {
+import { Person } from "@mui/icons-material"
+import InputField from "../components/form/InputField"
+
+export default function LoginPage() {
     return (
         <div className="flex items-center justify-center h-[100vh]">
-            <div className="flex flex-col items-center rounded-xl border-gray-300 border-2 w-[60%] min-w-[300px] max-w-[500px] h-[50%] shadow-lg">
-
+            <div className="flex flex-col items-center rounded-xl border-gray-300 border w-[60%] min-w-[300px] max-w-[500px] h-[50%] shadow-lg py-2">
+                <p className="text-2xl flex items-center gap-2"><Person fontSize="large" /> Login to RailBeam</p>
+                <form action="" className="flex flex-col items-center gap-4 pt-4">
+                    <InputField name="username" placeholder="Username" />
+                    <InputField name="password" placeholder="Password" type="password" />
+                </form>
             </div>
         </div>
     )
