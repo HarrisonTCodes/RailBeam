9c090ca0ca213ab9f97e0cc4e16018f2a316bab2
HarrisonTCodes <htillbackup@gmail.com>
2024-06-07 19:03:35 +0100

    Cleaned padding code

--- frontend/app/components/navbar/NavButton.tsx
+++ frontend/app/components/navbar/NavButton.tsx
@@ -4,7 +4,7 @@ import NavOption from "@/app/interfaces/NavOption";
 
 export default function NavButton({ link, label, icon } : NavOption) {
     return (
-        <Link href={link} title={label} className="hidden w-16 sm:flex md:w-32 flex-row justify-center items-center rounded-xl py-1 px-1 border-2 hover:bg-gray-100 transition gap-2">
+        <Link href={link} title={label} className="hidden w-16 sm:flex md:w-32 flex-row justify-center items-center rounded-xl p-1 border-2 hover:bg-gray-100 transition gap-2">
             {icon}
             <p className="hidden md:block">{label}</p>
         </Link>
