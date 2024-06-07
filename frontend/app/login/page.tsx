fb56dc3f76385ef6de78b9341f7fee817c2a7c35
HarrisonTCodes <htillbackup@gmail.com>
2024-06-07 17:49:09 +0100

    Initial login form box created

---
+++ frontend/app/login/page.tsx
@@ -0,0 +1,9 @@
+export default function Login() {
+    return (
+        <div className="flex items-center justify-center h-[100vh]">
+            <div className="flex flex-col items-center rounded-xl border-gray-300 border-2 w-[60%] min-w-[300px] max-w-[500px] h-[50%] shadow-lg">
+
+            </div>
+        </div>
+    )
+}
