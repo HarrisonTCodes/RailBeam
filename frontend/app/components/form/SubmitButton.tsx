afba8a573cab4bd6c8a457e0995b3a6be41ee9dd
HarrisonTCodes <htillbackup@gmail.com>
2024-06-07 19:14:53 +0100

    Changed submit button styling

--- frontend/app/components/form/SubmitButton.tsx
+++ frontend/app/components/form/SubmitButton.tsx
@@ -2,7 +2,7 @@ import { ReactElement } from "react";
 
 export default function SubmitButton({ label, icon } : { label: string, icon?: ReactElement }) {
     return (
-        <button type="submit" className="w-32 flex justify-center items-center rounded-xl p-1 border-2 hover:bg-gray-100 transition gap-2">
+        <button type="submit" className="text-lg w-72 flex justify-center items-center rounded-xl p-1 border-2 hover:bg-gray-100 transition gap-4">
             {icon}
             {label}
         </button>
