8c793e3c838b03442337df31fd47e70a40ecbc33
HarrisonTCodes <htillbackup@gmail.com>
2024-06-07 23:45:02 +0100

    Removed login/register and account references (migration to accountless)

--- frontend/app/components/navbar/Navbar.tsx
+++ frontend/app/components/navbar/Navbar.tsx
@@ -11,7 +11,6 @@ export default function Navbar() {
     const buttonData = [
         {link:"/", label:"Search", icon:<Search fontSize="large" />},
         {link:"/journeys", label:"Journeys", icon:<Bookmarks fontSize="large" />},
-        {link:"/account", label:"Account", icon:<Person fontSize="large" />}
     ]
 
     return (
