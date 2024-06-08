c1bc221ee01caec3b2209863f837a34fc79b6099
HarrisonTCodes <htillbackup@gmail.com>
2024-06-08 23:25:20 +0100

    Header and search bar added

--- frontend/app/journeys/page.tsx
+++ frontend/app/journeys/page.tsx
@@ -1,5 +1,22 @@
+"use client";
+
+import { useState } from "react"
+
 export default function JourneysPage() {
+    const [journeySearch, setJourneySearch] = useState("")
+
     return (
-        <></>
+        <>
+            <p className="pt-32 pb-8 text-center text-3xl font-semibold text-primary">Train Journeys</p>
+            <div className="pb-8 px-2 flex flex-col md:flex-row justify-center items-center gap-8">
+                <input 
+                    type="text"
+                    placeholder="Search journeys"
+                    className="border-2 border-gray-300 focus:border-secondary focus:outline-none rounded-xl focus:ring-0 py-2 px-2 text-xl w-80 transition"
+                    onChange={(ev) => setJourneySearch(ev.target.value)}
+                    value={journeySearch}
+                />
+            </div>
+        </>
     )
 }
