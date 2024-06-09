ec5aa47c1fe45aa4142e989ec024c0b803d6de55
HarrisonTCodes <htillbackup@gmail.com>
2024-06-09 15:17:08 +0100

    Added journey element styling

--- frontend/app/components/journey/Journey.tsx
+++ frontend/app/components/journey/Journey.tsx
@@ -4,12 +4,13 @@ import { Delete, OpenInNew } from "@mui/icons-material";
 
 export default function Journey({ name, firstStation, secondStation } : JourneyInfo) {
     return (
-        <div className="flex justify-between border border-gray-300 rounded-xl p-2 max-w-[810px] w-[90%] bg-white">
-            <section>
+        <div className="flex justify-between border border-gray-300 rounded-xl p-2 max-w-[600px] w-[90%] bg-white">
+            <section className="max-w-[60%]">
                 <p className="text-2xl font-medium text-primary">{name}</p>
-                <p>Live trains between {firstStation} and {secondStation}</p>
+                <p className="text-gray-400">{firstStation}</p>
+                <p className="text-gray-400">{secondStation}</p>
             </section>
-            <section className="flex gap-4">
+            <section className="flex items-center gap-4">
                 <SmallButton icon={<OpenInNew fontSize="medium" htmlColor="#ffffff" />} />
                 <SmallButton icon={<Delete fontSize="medium" htmlColor="#ffffff" />} />
             </section>
