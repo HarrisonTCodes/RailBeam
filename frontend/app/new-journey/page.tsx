5cb6bb75cddf02e1406ce278658e17fe450f0557
HarrisonTCodes <htillbackup@gmail.com>
2024-06-09 19:45:54 +0100

    Added back button

--- frontend/app/new-journey/page.tsx
+++ frontend/app/new-journey/page.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react"
 import SearchBar from "../components/search/SearchBar"
-import { Add } from "@mui/icons-material";
+import { Add, ArrowBack } from "@mui/icons-material";
 import { useRouter } from "next/navigation";
 
 export default function NewJourney() {
@@ -35,7 +35,12 @@ export default function NewJourney() {
     return (
         <div className="flex items-center justify-center h-[100vh]">
             <div className="flex flex-col items-center rounded-xl border-gray-300 border w-[80%] min-w-[350px] max-w-[500px]  shadow-lg py-2 gap-4">
-                <p className="text-3xl flex items-center gap-2 text-primary font-semibold pt-2 pb-8">Create New Journey</p>
+                <p className="text-3xl flex items-center gap-2 text-primary font-semibold pt-2 pb-8">
+                    <button onClick={() => router.push("/journeys")}>
+                        <ArrowBack fontSize="large" htmlColor="#142c8e" />
+                    </button>
+                    Create New Journey
+                </p>
                 <input 
                     placeholder="Journey name"
                     className="border-2 border-gray-300 focus:border-secondary focus:outline-none rounded-xl focus:ring-0 py-2 px-2 text-xl w-80 transition"
