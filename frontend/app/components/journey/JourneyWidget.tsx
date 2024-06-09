49560f3a536a96c15038b7c25b2ae454fcea1106
HarrisonTCodes <htillbackup@gmail.com>
2024-06-09 19:49:42 +0100

    Added functionality

--- frontend/app/components/journey/JourneyWidget.tsx
+++ frontend/app/components/journey/JourneyWidget.tsx
@@ -2,7 +2,7 @@ import JourneyInfo from "@/app/interfaces/JourneyInfo";
 import SmallButton from "../search/SmallButton";
 import { Delete, OpenInNew } from "@mui/icons-material";
 
-export default function JourneyWidget({ name, firstStation, secondStation } : JourneyInfo) {
+export default function JourneyWidget({ name, firstStation, secondStation, deleteFunction, openFunction } : { name: string, firstStation: string, secondStation: string, deleteFunction: () => void, openFunction: () => void }) {
     return (
         <div className="flex justify-between border border-gray-300 rounded-xl p-2 max-w-[600px] w-[90%] bg-white">
             <section className="max-w-[60%]">
@@ -11,8 +11,8 @@ export default function JourneyWidget({ name, firstStation, secondStation } : Jo
                 <p className="text-gray-400">{secondStation}</p>
             </section>
             <section className="flex items-center gap-4">
-                <SmallButton icon={<OpenInNew fontSize="medium" htmlColor="#ffffff" />} />
-                <SmallButton icon={<Delete fontSize="medium" htmlColor="#ffffff" />} />
+                <SmallButton icon={<OpenInNew fontSize="medium" htmlColor="#ffffff" />} onClick={openFunction} />
+                <SmallButton icon={<Delete fontSize="medium" htmlColor="#ffffff" />} onClick={deleteFunction} />
             </section>
         </div>
     )
